dd230d2f44a150913c2dfc9405f846d4f53276b4
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-04-18 14:14:30 +0530

    added footer and styling to the page

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
   const fetchNFTs = async () => {
     let nfts;
     console.log("fetching nfts");
-    // const api_key = "7ELKDXZo6XQKNRg1vwJT26Fu_ziS8tnL"
     const api_key = import.meta.env.VITE_API_KEY
     const baseURL = `https://eth-mainnet.g.alchemy.com/v2/${api_key}/getNFTs/`;
     var requestOptions = {
@@ -53,37 +52,34 @@ function App() {
     }
   };
 
-
-
   return (
-    <div className="flex flex-col items-center justify-center py-8 gap-y-3">
-      <div className="flex flex-col w-full justify-center items-center gap-y-2">
-        <input
+    <div className='bg-black min-h-screen text-white flex flex-col items-center justify-center py-8 gap-y-3'>
+      <div className='flex flex-col w-full justify-center items-center gap-y-2'>
+        <input className='px-3 py-3 w-1/3 text-gray-700 placeholder-blueGray-300 relative bg-white rounded text-md border-0 shadow outline-none focus:outline-none focus:ring '
           disabled={fetchForCollection}
           onChange={(e) => { setWalletAddress(e.target.value) }}
-          value={wallet} type={"text"}
-          placeholder="Add your wallet address"></input>
+          value={wallet}
+          type="text" placeholder='Add your wallet address' />
         <input
+          className='px-3 py-3 w-1/3 text-gray-700 placeholder-blueGray-300 relative bg-white rounded text-md border-0 shadow outline-none focus:outline-none focus:ring '
           onChange={(e) => { setCollectionAddress(e.target.value) }}
-          value={collection} type={"text"}
-          placeholder="Add the collection address"></input>
-        <label className="text-gray-600 ">
-          <input
+          valu={collection} type="text" placeholder='Add the collection address' />
+        <label className='text-gray-100'>
+          <input type={"checkbox"}
+            className='px-3 py-3 text-gray-600 placeholder-blueGray-300 relative bg-white rounded text-md border-0 shadow outline-none focus:outline-none focus:ring w-fit'
             onChange={(e) => { setFetchForCollection(e.target.checked) }}
-            type={"checkbox"} className="mr-2"></input>
+          />
           Fetch for collection
         </label>
-        <button
-          className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"}
+        <button className={'disabled:bg-blue-600  text-white bg-blue-500 active:bg-blue-800 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'}
           onClick={
             () => {
               if (fetchForCollection) {
                 fetchNFTsForCollection();
               } else fetchNFTs();
             }
-          }>
-          Let's go!
-        </button>
+          }
+        >Let's go!</button>
       </div>
       <div className='flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center'>
         {
@@ -94,7 +90,16 @@ function App() {
           })
         }
       </div>
+      <footer className='w-full pb-4 px-2 py-2 md:px-4'>
+        <p className='text-center text-base text-gray-400 font-semibold'>
+          Made by {" "}
+          <a className="underline text-blue-300 hover:text-blue-500" target="_blank" href="https://github.com/Deepjyoti-Sarmah">
+            @Deepjyoti Sarmah
+          </a>
+        </p>
+      </footer>
     </div>
+
   )
 
 }
--- src/components/nftCard.jsx
+++ src/components/nftCard.jsx
@@ -1,56 +1,16 @@
-// export const NFTCard = ({ nft }) => {
-
-//     return (
-//         // <div className="w-1/4 flex flex-col my-10 bg-gray-800 p-5 rounded-md ">
-//         //     <div className="rounded-md">
-//         //         <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway} ></img>
-//         //     </div>
-//         //     <div className="flex flex-col y-gap-2 px-2 py-3 bg-gray-800 rounded-b-md ">
-//         //         <div className=" flex flex-wrap justify-between items-center text-md mt-3">
-//         //             <h2 className=" flex text-md text-gray-100 font-bold mt-3">{nft.title}</h2>
-//         //             <p className="flex text-gray-200 text-sm font-semibold mb-2">Id: {nft.id.tokenId}</p>
-//         //             <p className="flex justify-between items-center text-gray-300 text-sm font-semibold mb-2">{nft.contract.address}</p>
-//         //         </div>
-
-//         //         <div className="flex flex-grow mt-2 justify-between items-center text-sm">
-//         //             <p className="text-gray-400 text-sm mb-2">{nft.description}</p>
-//         //         </div>
-//         //     </div>
-
-//         // </div>
-//         <div class="max-w-sm my-10 p-5 rounded overflow-hidden shadow-lg bg-gray-900">
-//             <img className="w-full rounded-md" src={nft.media[0].gateway} />
-//             <div className="px-6 py-4 rounded-md">
-//                 <div className="font-bold text-xl mb-2">{nft.title}</div>
-//                 {/* <div className="font-semibold text-sm mb-2">Id: {nft.id.tokenId}</div> */}
-//                 <div className="font-semibold text-sm  text-gray-200 mb-2">{nft.contract.address}</div>
-//                 <p className="text-gray-400 text-base">
-//                     {nft.description}
-//                 </p>
-//             </div>
-//         </div>
-//     );
-// }
-
 export const NFTCard = ({ nft }) => {
 
     return (
-        <div className="w-1/4 flex flex-col ">
-            <div className="rounded-md">
-                <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway} ></img>
+        <div className="max-w-sm my-10 p-5 rounded overflow-hidden shadow-lg bg-gray-900">
+            <img className="w-full rounded-md" src={nft.media[0].gateway} />
+            <div className="px-6 py-4 rounded-md">
+                <div className="font-bold text-xl text-gray-100 mb-2">{nft.title}</div>
+                {/* <div className="font-semibold text-sm mb-2">Id: {nft.id.tokenId}</div> */}
+                <div className="font-semibold text-sm  text-gray-200 mb-2">{nft.contract.address}</div>
+                <p className="text-gray-400 text-base">
+                    {nft.description}
+                </p>
             </div>
-            <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110 ">
-                <div className="">
-                    <h2 className="text-xl text-gray-800">{nft.title}</h2>
-                    <p className="text-gray-600">Id: {nft.id.tokenId}</p>
-                    <p className="text-gray-600" >{nft.contract.address}</p>
-                </div>
-
-                <div className="flex-grow mt-2">
-                    <p className="text-gray-600">{nft.description}</p>
-                </div>
-            </div>
-
         </div>
-    )
+    );
 }
